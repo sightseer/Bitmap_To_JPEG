4e4988b5fe85cccf45c6a21c0849af9c8a0b7930
sightseer-xu <xuynktw@gmail.com>
2018-06-23 00:05:55 +0800

    Add header part of JPEG

--- BMP_To_JPEG.c
+++ BMP_To_JPEG.c
@@ -25,7 +25,7 @@ unsigned short ColorDifference_quantization_matrix[BLOCK_LEN][BLOCK_LEN] = {
 	{99,  99,  99,  99,  99,  99,  99,  99},
 	{99,  99,  99,  99,  99,  99,  99,  99}
 };
-
+unsigned char luminance_quantization_arr[BLOCK_LEN*BLOCK_LEN],ColorDifference_quantization_arr[BLOCK_LEN*BLOCK_LEN];
 unsigned char luminance_DC_code_length[] = {0x00, 0x01, 0x05, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; 
 unsigned char luminance_DC_value[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B};
 
@@ -78,7 +78,7 @@ typedef struct AC_Code_Info
 	unsigned short HM_code_len,bits_len;
 }AC_code_Len;
 
-void RGB_To_YCrCb(unsigned char **Y, unsigned char **Cr, unsigned char **Cb, unsigned char *rgb_data);
+void RGB_To_YCrCb(unsigned char **Y, unsigned char **Cr, unsigned char **Cb, unsigned char *rgb_data, int height, int width);
 void DCT(int x,int y, unsigned char **data, float **DCT_res);
 void Quantize(float **data, short **quantize_res, int is_luminance);
 void Build_Huffman_Table_for_All();
@@ -87,10 +87,12 @@ void ZigZag_Scan(short **quantize_res, short *color_data_arr);
 unsigned short DC_Encode(short diff_DC, int is_luminance);
 unsigned short REL_Encode(short *color_data_arr, REL_coding_data *rel_data);
 AC_code_Len AC_Encode(unsigned short zero_num, short color_data, unsigned char *HM_codeword, unsigned char *real_data_bitstring, int is_luminance);
+void Encode_Jpeg_Header(FILE *fp);
 
 int main()
 {
 		FILE *input_fp = fopen("Lenna.bmp","rb");
+//		int width,height;
 		if(input_fp == NULL)
 		{
 			perror("FAIL TO OPEN FILE");
@@ -171,7 +173,7 @@ int main()
 		for(int i=0;i<height;i++)
 			Cb[i]=(unsigned char*)malloc(width*sizeof(unsigned char));
 
-    RGB_To_YCrCb(Y,Cr,Cb,rgb_data);
+    RGB_To_YCrCb(Y,Cr,Cb,rgb_data, height, width);
 
     //allocate and initialize the block
     float **DCT_res;
@@ -192,45 +194,50 @@ int main()
 
 
     Build_Huffman_Table_for_All();
-    short last_DC=0;
+    short Y_last_DC = 0, Cr_last_DC = 0, Cb_last_DC = 0;
+    FILE *output_fp = fopen("out.jpg","wb");
+    Encode_Jpeg_Header(output_fp);
+
     for(int i=0;i<height;i+=BLOCK_LEN)
       for(int j=0;j<width;j+=BLOCK_LEN)
       {
       	unsigned short DC_code;
       	unsigned short pair_num;
       	unsigned char *HM_codeword, *real_data_bitstring;
-        HM_codeword = (unsigned char*)malloc(16*sizeof(unsigned char));
+        short diff_DC;
+	int is_luminance = 1;
+	HM_codeword = (unsigned char*)malloc(16*sizeof(unsigned char));
         real_data_bitstring = (unsigned char*) malloc(16*sizeof(unsigned char));
-
+/*
+// Y data transform
         DCT(i,j,Y,DCT_res);
-        Quantize(DCT_res, quantize_res, 1);
+        Quantize(DCT_res, quantize_res, is_luminance );
         ZigZag_Scan(quantize_res, color_data_arr);
-
-        for(int ii=0; ii<64; ii++)
-        	printf("i=%d %d\n", ii, color_data_arr[ii]);
-        short diff_DC = color_data_arr[0]-last_DC;
-        last_DC = color_data_arr[0];
-         
-        DC_code = DC_Encode(diff_DC, 1);  
+        
+//	for(int ii=0; ii<64; ii++)
+//        	printf("i=%d %d\n", ii, color_data_arr[ii]);
+        	
+	diff_DC = color_data_arr[0]-y_last_DC;
+        Y_last_DC = color_data_arr[0];
+        DC_code = DC_Encode(diff_DC,is_luminance );  
         pair_num = REL_Encode(color_data_arr, rel_data);
   
         printf("DC_code=%u pair_num=%u\n", DC_code, pair_num);
-  		pair_num = 1;
+  	//	pair_num = 1;
         for(int v=0; v<pair_num; v++)
         {
-        	printf("zero_num=%u data=%d\n", rel_data[v].zero_num,rel_data[v].color_data);
+//        	printf("zero_num=%u data=%d\n", rel_data[v].zero_num,rel_data[v].color_data);
         	//initialize HM_codeword, real_data_bitstring
         	for(int k=0; k<16; k++)
         	{
         		HM_codeword[k] = NULL;
         		real_data_bitstring[k] = NULL;
         	}
-        	AC_code_Len AC_code_len = AC_Encode(rel_data[v].zero_num, rel_data[v].color_data, HM_codeword, real_data_bitstring, 1);
+        	AC_code_Len AC_code_len = AC_Encode(rel_data[v].zero_num, rel_data[v].color_data, HM_codeword, real_data_bitstring, is_luminance );
         
         
         	int jj = 0;
-        	printf("FM_code=");
-               	
+        	printf("FM_code=");  	
         //	while(HM_codeword[jj]!='\0')
         	while(jj < AC_code_len.HM_code_len)
            	{
@@ -244,21 +251,107 @@ int main()
         	{
         			printf("%x", real_data_bitstring[jj]);        		
         			jj++;
-            }
+            	}
           	printf("\n");
          
         }
-        
-/*
-        for(int i=0;i<height;i++)
-            {
-                for(int j=0;j<width;j++)
-                {
-                    printf(" %d",quantize_res[i][j]);
-                }
-                printf("\n");
-            }
+
 */
+	is_luminance = 0;
+
+
+// Cr data trnasform
+	DCT(i,j,Cr,DCT_res);
+        Quantize(DCT_res, quantize_res, is_luminance );
+        ZigZag_Scan(quantize_res, color_data_arr);
+
+//	for(int ii=0; ii<64; ii++)
+//        	printf("i=%d %d\n", ii, color_data_arr[ii]);
+     
+        diff_DC = color_data_arr[0] - Cr_last_DC;
+        Cr_last_DC = color_data_arr[0];
+        DC_code = DC_Encode(diff_DC, is_luminance);  
+        pair_num = REL_Encode(color_data_arr, rel_data);
+        
+	printf("DC_code=%u pair_num=%u\n", DC_code, pair_num);
+	for(int v=0; v<pair_num; v++)
+        {
+//        	printf("zero_num=%u data=%d\n", rel_data[v].zero_num,rel_data[v].color_data);
+        	//initialize HM_codeword, real_data_bitstring
+        	for(int k=0; k<16; k++)
+        	{
+        		HM_codeword[k] = NULL;
+        		real_data_bitstring[k] = NULL;
+        	}
+        	AC_code_Len AC_code_len = AC_Encode(rel_data[v].zero_num, rel_data[v].color_data, HM_codeword, real_data_bitstring,is_luminance );
+	 	int jj = 0;
+        	printf("FM_code=");  	
+        //	while(HM_codeword[jj]!='\0')
+        	while(jj < AC_code_len.HM_code_len)
+           	{
+        		//if(HM_codeword[jj]!=NULL)
+        			printf("%x", HM_codeword[jj]);		
+        			jj++;
+        	}
+        	printf(" bits=");
+        	jj = 0;
+        	while(jj < AC_code_len.bits_len)
+        	{
+        			printf("%x", real_data_bitstring[jj]);        		
+        			jj++;
+            	}
+          	printf("\n");
+        
+        }
+
+
+/*
+// Cb data transform
+	DCT(i,j,Cb,DCT_res);
+        Quantize(DCT_res, quantize_res, is_luminance );
+        ZigZag_Scan(quantize_res, color_data_arr);
+
+        diff_DC = color_data_arr[0] - Cb_last_DC;
+        Cb_last_DC = color_data_arr[0];
+        DC_code = DC_Encode(diff_DC, is_luminance );  
+        pair_num = REL_Encode(color_data_arr, rel_data);
+	
+	printf("DC_code=%u pair_num=%u\n", DC_code, pair_num);
+	for(int v=0; v<pair_num; v++)
+        {
+//        	printf("zero_num=%u data=%d\n", rel_data[v].zero_num,rel_data[v].color_data);
+        	//initialize HM_codeword, real_data_bitstring
+        	for(int k=0; k<16; k++)
+        	{
+        		HM_codeword[k] = NULL;
+        		real_data_bitstring[k] = NULL;
+        	}
+        	AC_code_Len AC_code_len = AC_Encode(rel_data[v].zero_num, rel_data[v].color_data, HM_codeword, real_data_bitstring, is_luminance );
+
+	 	int jj = 0;
+        	printf("FM_code=");  	
+        //	while(HM_codeword[jj]!='\0')
+        	while(jj < AC_code_len.HM_code_len)
+           	{
+        		//if(HM_codeword[jj]!=NULL)
+        			printf("%x", HM_codeword[jj]);		
+        			jj++;
+        	}
+        	printf(" bits=");
+        	jj = 0;
+        	while(jj < AC_code_len.bits_len)
+        	{
+        			printf("%x", real_data_bitstring[jj]);        		
+        			jj++;
+            	}
+          	printf("\n");
+ 
+        }
+*/
+	free(HM_codeword);
+        free(real_data_bitstring);
+
+
       }
 
 		/*
@@ -272,7 +365,7 @@ int main()
 }
 
 
-void RGB_To_YCrCb(unsigned char **Y, unsigned char **Cr, unsigned char **Cb, unsigned char *rgb_data)
+void RGB_To_YCrCb(unsigned char **Y, unsigned char **Cr, unsigned char **Cb, unsigned char *rgb_data, int height, int width)
 {
 		for(int i=0;i<height;i++)
 				{
@@ -353,20 +446,21 @@ void Quantize(float **data, short **quantize_res, int is_luminance)
         for(int j=0;j<BLOCK_LEN;j++)
             printf(" %d", quantize_res[i][j]);
          printf("\n");
-        }    
+        }
+
     return ;
 }
 
 void Build_Huffman_Table_for_All()
 {
 	Build_Huffman_Table(luminance_AC_code_length, luminance_AC_value, luminance_AC_huffman_table);
-	/*
-	for(int i=0; i<30; i++)
-	{
-		printf("HM_i=%d %u\n", i, luminance_AC_huffman_table[i]);
-	}
-	*/
+	
+//	for(int i=0; i<10; i++)
+//		printf("HM_i=%d %u\n", i, luminance_AC_huffman_table[i]);
+
 	Build_Huffman_Table(chrominance_AC_code_length, chrominance_AC_value, chrominance_AC_huffman_table);
+//	for(int i=0;i<10;i++)
+//		printf("i=%d Chrominance_AC=%u\n",i,chrominance_AC_huffman_table[i]);
 	Build_Huffman_Table(luminance_DC_code_length, luminance_DC_value, luminance_DC_huffman_table);
 	Build_Huffman_Table(chrominance_DC_code_length, chrominance_DC_value, chrominance_DC_huffman_table);
 }
@@ -390,10 +484,10 @@ void Build_Huffman_Table(unsigned char *code_length, unsigned char *code_value, 
 void ZigZag_Scan(short **quantize_res, short *color_data_arr)
 {
 
-unsigned short scan_x[] = {0,0,1,2,1,0,0,1,2,3,4,3,2,1,0,0,1,2,3,4,5,6,5,4,3,2,1,0,0,1,2,3,4,
+	unsigned short scan_x[] = {0,0,1,2,1,0,0,1,2,3,4,3,2,1,0,0,1,2,3,4,5,6,5,4,3,2,1,0,0,1,2,3,4,
 	5,6,7,7,6,5,4,3,2,1,2,3,4,5,6,7,7,6,5,4,3,4,5,6,7,7,6,5,6,7,7 };
 
-unsigned short scan_y[] = {0,1,0,0,1,2,3,2,1,0,0,1,2,3,4,5,4,3,2,1,0,0,1,2,3,4,5,6,7,6,5,4,3,
+	unsigned short scan_y[] = {0,1,0,0,1,2,3,2,1,0,0,1,2,3,4,5,4,3,2,1,0,0,1,2,3,4,5,6,7,6,5,4,3,
 	2,1,0,1,2,3,4,5,6,7,7,6,5,4,3,2,3,4,5,6,7,7,6,5,4,5,6,7,7,6,7 };
 
 	unsigned short arr_len = BLOCK_LEN*BLOCK_LEN;
@@ -484,7 +578,7 @@ AC_code_Len AC_Encode(unsigned short zero_num, short color_data, unsigned char *
 				real_data_bitstring[j-i-1] = temp;
 			}
 
-		printf("j=%u\n", j);
+//		printf("j=%u\n", j);
 
 		unsigned short index_in_table = zero_num*16+j;
 		unsigned short HM_value;
@@ -495,6 +589,19 @@ AC_code_Len AC_Encode(unsigned short zero_num, short color_data, unsigned char *
 //		printf("index_in_table=%u HM_value=%u\n", index_in_table, HM_value);
 		//unsigned char temp_bit[17];
 		unsigned short bit_len=0;
+		if(HM_value == 0)
+		{
+			HM_codeword[0] = 0;
+			HM_codeword[1] = 0;
+			bit_len = 2;
+		}
+		else if(HM_value == 1)
+		{
+			HM_codeword[0] = 1;
+			HM_codeword[1] = 0;
+			bit_len = 2;			
+		}
+		else
 		while(HM_value)
 		{
 			HM_codeword[bit_len] = HM_value & 1;
@@ -525,4 +632,106 @@ AC_code_Len AC_Encode(unsigned short zero_num, short color_data, unsigned char *
 		printf("\n");
 */
 		return code_len;
+}
+
+void Encode_Jpeg_Header(FILE *fp)
+{
+	//SOI
+	write_func(0xFFD8,fp,2);
+
+	//APPO
+	write_func(0xFFE0,fp,2);
+	write_func(16,fp,2);
+	write_func("JFIF",fp,5);
+
+	write_func(0x1,fp,1);
+	write_func(0x1,fp,1);
+	write_func(0x0,fp,1);
+	write_func(0x1,fp,2);
+	write_func(0x1,fp,2);
+	write_func(0x0,fp,1);
+	write_func(0x0,fp,1);
+
+
+    //DQT
+    for(int i=0;i<BLOCK_LEN;i++)
+        for(int j=0;j<BLOCK_LEN;j++)
+        {
+            luminance_quantization_arr[i*BLOCK_LEN+j] = luminance_quantization_matrix[i][j];
+            ColorDifference_quantization_arr[i*BLOCK_LEN+j] = ColorDifference_quantization_matrix[i][j];
+        }
+	write_func(0xFFDB, fp);		//marker = 0xFFDB
+	write_func(132, fp);			//size=132
+	write_func(0x0, fp);			//QTYinfo== 0:  bit 0..3: number of QT = 0 (table for Y)
+									//				bit 4..7: precision of QT
+									//				bit 8	: 0
+	write_func(luminance_quantization_arr, fp, 64);		//YTable
+	write_func(1, fp);			//QTCbinfo = 1 (quantization table for Cb,Cr)
+	write_func(ColorDifference_quantization_arr, fp, 64);	//CbCrTable
+
+	
+	//SOFO
+	write_func(0xFFC0, fp, 2);			//marker = 0xFFC0
+	write_func(17, fp, 2);				//length = 17 for a truecolor YCbCr JPG
+	write_func(0x8, fp);				//precision = 8: 8 bits/sample
+	write_func(height&0xFFFF, fp, 2);	//height
+	write_func(width&0xFFFF, fp, 2);	//width
+	write_func(0x3, fp, 1);				//nrofcomponents = 3: We encode a truecolor JPG
+
+	write_func(0x1, fp, 1);				//IdY = 1
+	write_func(0x11, fp);				//HVY sampling factors for Y (bit 0-3 vert., 4-7 hor.)(SubSamp 1x1)
+	write_func(0x0, fp, 1);				//QTY  Quantization Table number for Y = 0
+
+	write_func(0x2, fp, 1);				//IdCb = 2
+	write_func(0x11, fp, 1);				//HVCb = 0x11(SubSamp 1x1)
+	write_func(0x1, fp, 1);				//QTCb = 1
+
+	write_func(0x3, fp, 1);				//IdCr = 3
+	write_func(0x11, fp, 1);				//HVCr = 0x11 (SubSamp 1x1)
+	write_func(0x1, fp, 1);				//QTCr Normally equal to QTCb = 1
+
+	//DHT
+	write_func(0xFFC4, fp, 2);		// marker = 0xFFC4
+	write_func(0x01A2, fp, 2);		// length = 0x01A2
+	write_func(0x0, fp, 1);			// HTYDCinfo bit 0..3	: number of HT (0..3), for Y =0
+									// bit 4 : type of HT, 0 = DC table,1 = AC table
+									// bit 5..7	: not used, must be 0
+	write_func(luminance_DC_code_length, fp, sizeof(luminance_DC_code_length));	// DC_L_NRC
+	write_func(luminance_DC_value, fp, sizeof(luminance_DC_value));	// DC_L_VALUE
+	write_func(0x10, fp);			// HTYACinfo
+	write_func(luminance_AC_code_length, fp, sizeof(luminance_AC_code_length));
+	write_func(luminance_AC_value, fp, sizeof(luminance_AC_value)); // we'll use the standard Huffman tables
+	write_func(0x01, fp);			// HTCbDCinfo
+	write_func(chrominance_DC_code_length, fp, sizeof(chrominance_DC_code_length));
+	write_func(chrominance_DC_value, fp, sizeof(chrominance_DC_value));
+	write_func(0x11, fp);			// HTCbACinfo
+	write_func(chrominance_AC_code_length, fp, sizeof(chrominance_AC_code_length));
+	write_func(chrominance_AC_value, fp, sizeof(chrominance_AC_value));
+
+	//SOS
+	write_func(0xFFDA, fp, 2);		//marker = 0xFFC4
+	write_func(12, fp, 2);			//length = 12
+	write_func(0x3, fp, 1);			//nrofcomponents, Should be 3: truecolor JPG
+
+	write_func(1, fp, 1);			//Idy=1
+	write_func(0, fp, 1);			//HTY	bits 0..3: AC table (0..3)
+									//		bits 4..7: DC table (0..3)
+	write_func(2, fp, 1);			//IdCb
+	write_func(0x11, fp, 1);			//HTCb
+
+	write_func(3, fp, 1);			//IdCr
+	write_func(0x11, fp, 1);			//HTCr
+
+	write_func(0, fp, 1);			//Ss not interesting, they should be 0,63,0
+	write_func(0x3F, fp, 1);			//Se
+	write_func(0, fp, 1);			//Bf
+	
+}
+
+void write_func(FILE *fp, unsigned short k, int byte_size)
+{
+    //    unsigned char temp_k;
+    //    if(byte_size == 1)
+
+		fwrite(k,1,byte_size,f);	
 }
